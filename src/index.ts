151bfd64d093b611806f434b731fbe96ce1e5e06
Jordan SABLON <jordan.sablon@datalegaldrive.com>
2024-02-16 18:33:52 +0100

    Configure connection to database

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 import { router } from "./routes/students.route";
+
+dotenv.config();
 
 const hostname = "127.0.0.1";
 const port = 5000;
+
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}/${process.env.DB_NAME}?retryWrites=true&w=majority`
+);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
 
 const app = express();
 app.use(express.json());
